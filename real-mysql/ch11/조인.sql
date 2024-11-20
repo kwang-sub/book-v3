b5ccc65e0e8f8e6db764e3b65ea9030bcb9bf03c
kwang-sub <74045087+kwang-sub@users.noreply.github.com>
2024-11-20 22:12:23 +0900

    Add: json 관련 기능 학습

--- real-mysql/ch11/조인.sql
+++ real-mysql/ch11/조인.sql
@@ -2,3 +2,30 @@ explain
 select *
 from employees e, dept_emp de
 where e.emp_no = de.emp_no;
+
+
+create table tb_test1
+(
+    user_id   int,
+    user_type int,
+    primary key (user_id)
+);
+
+create table tb_test2
+(
+    user_type char(1),
+    type_desc varchar(10),
+    primary key (user_type)
+);
+
+explain
+select *
+from tb_test1 tb1,
+     tb_test2 tb2
+where tb1.user_type = tb2.user_type;
+
+explain
+select *
+from employees e
+         left join dept_emp de on de.emp_no = e.emp_no
+         left join departments d on d.dept_no = de.dept_no and d.dept_name = 'Development';
