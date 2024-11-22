0a57c1e4f356619ef1317751c5fd8d1391301aff
kwang-sub <74045087+kwang-sub@users.noreply.github.com>
2024-11-22 22:50:34 +0900

    Add: cte 학습

--- real-mysql/ch11/CTE.sql
+++ real-mysql/ch11/CTE.sql
@@ -1,6 +1,7 @@
 explain
 with cte1 as (select * from departments)
-select * from cte1;
+select *
+from cte1;
 
 explain
 select cte1.*
@@ -14,4 +15,55 @@ from employees e
          join cte1 t2 on t2.emp_no = e.emp_no;
 
 with recursive cte(no) as (select 1 union all select (no + 1) from cte where no < 5)
-select * from cte;
+select *
+from cte;
+
+with cte1 as (select * from departments)
+select *
+from cte1;
+
+with cte1 (fd1, fd2, fd3) as (select * from departments)
+select *
+from cte1;
+
+create table test_employees
+(
+    id         int primary key not NULL,
+    name       varchar(100)    not null,
+    manager_id int             null,
+    index (manager_id),
+    foreign key (manager_id) references test_employees (id)
+);
+
+insert into test_employees
+values (333, "Yasmina", null),
+       (198, "John", 333),
+       (692, "Tarek", 333),
+       (29, "Pedro", 198),
+       (4610, "Sarah", 29),
+       (72, "Pierre", 29),
+       (123, "Adil", 692);
+
+with recursive managers as (select *, 1 as lv
+                            from test_employees
+                            where id = 123
+                            union all
+                            select e.*, lv + 1
+                            from managers m
+                                     inner join test_employees e on e.id = m.manager_id and m.manager_id is not null)
+select *
+from managers
+order by lv desc;
+
+with recursive managers as (select *, cast(id as char(100)) as manager_path, 1 as lv
+                            from test_employees
+                            where manager_id is null
+                            union all
+                            select e.*,
+                                   concat(e.id, '->', m.manager_path) as manager_path,
+                                   lv + 1
+                            from managers m
+                                     inner join test_employees e on e.manager_id = m.id)
+select *
+from managers
+order by lv asc;
