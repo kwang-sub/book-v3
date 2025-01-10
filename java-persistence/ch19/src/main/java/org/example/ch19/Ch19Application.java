3c36ddea6392dc2d23d0a830d04df8c495f01e41
kwang <chlrhkdtjq11@naver.com>
2025-01-10 19:31:27 +0900

    Add: QueryDSL 학습

---
+++ java-persistence/ch19/src/main/java/org/example/ch19/Ch19Application.java
@@ -0,0 +1,13 @@
+package org.example.ch19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch19Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch19Application.class, args);
+    }
+
+}
