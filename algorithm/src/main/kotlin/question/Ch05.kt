93d18d20523518c51a979e89652f70a24c3bfd3b
kwang <chlrhkdtjq11@naver.com>
2025-04-23 22:32:54 +0900

    Add: 비재귀함수 구현중

--- algorithm/src/main/kotlin/question/Ch05.kt
+++ algorithm/src/main/kotlin/question/Ch05.kt
@@ -1,11 +1,14 @@
 package question
 
+import java.util.*
+
 fun main() {
 //    println(q1(4))
 //    println(q2(3, 6))
 //    println(q2(5, 15))
 //    println(gcdArray(intArrayOf(12, 6, 9)))
-    println(gcdArrayV2(intArrayOf(12, 6, 3, 4), 0, 4))
+//    println(gcdArrayV2(intArrayOf(12, 6, 3, 4), 0, 4))
+    q5(1)
 }
 
 fun q1(n: Int): Int {
@@ -51,3 +54,33 @@ fun gcd(a: Int, b: Int): Int {
     if (b == 0) return a
     return gcd(b, a % b)
 }
+
+fun q5(n: Int) {
+    var a = n
+    var ptr = -1
+    var s = 0
+    val nStk = IntArray(1000)
+    val sStk = IntArray(1000)
+
+    while (true) {
+        if (a > 0) {
+            nStk[++ptr] = a
+            sStk[ptr] = s
+            if (s == 0) a -= 1
+            else if (s == 1) {
+                a = a - 2
+                s = 0
+            }
+            continue
+        }
+        do {
+            a = nStk[ptr]
+            s = sStk[ptr--] + 1
+            if (s == 2) {
+                println(a)
+                if (ptr < 0) return
+            }
+        } while (s == 2)
+    }
+}
+
